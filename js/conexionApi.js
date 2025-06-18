3831f2ce82d69a85e5ea1c663886bf617ef594b7
Driany2003 <creo07@hotmail.com>
2025-06-18 01:46:14 -0500

    mejoras en el texto, y presentacion

--- js/conexionApi.js
+++ js/conexionApi.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
    
     document.getElementById('descargar').addEventListener('click', function() {
-        const pdfUrl = './documento/GiomarOrtegaCV.pdf';
+        const pdfUrl = './documento/GiomarOrtega_CV.pdf';
         const link = document.createElement('a');
         link.href = pdfUrl;
         link.download = 'GiomarOrtegaCV.pdf';
